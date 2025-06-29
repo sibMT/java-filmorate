dd586d670b48d24658c469f1b5304a9bbfe897ca
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-06-30 01:12:29 +0700

    Спринт 12(v7)

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,15 +3,11 @@ package ru.yandex.practicum.filmorate.service;
 import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service

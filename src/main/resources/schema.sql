b046c59345dbd73b6faee4937b28ec0e78193a70
Tarasov Maxim <tarasovmaxxx@yandex.ru>
2025-06-30 03:02:26 +0700

    Спринт 12(v10)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS film_genres (
 CREATE TABLE IF NOT EXISTS friends (
     user_id BIGINT NOT NULL,
     friend_id BIGINT NOT NULL,
-    status BOOLEAN DEFAULT FALSE,
+    status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (user_id, friend_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (friend_id) REFERENCES users(user_id) ON DELETE CASCADE
